cfeabc9635c7636509392d016a96f30ccb71abd8
LuDelphic <sawa94378@gmail.com>
2023-02-08 20:31:07 +0300

    Initial Interface
    Added GetCoinList function

---
+++ Crypto-Currency/MarketClasses/CoinsList.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace Crypto_Currency.MarketClasses
+{
+    public class CoinsList
+    {
+        public string Name { get; set; }
+        public BitmapImage ImagePath { get; set; }
+    }
+}
---
+++ Crypto-Currency/MarketClasses/IMarkets.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Crypto_Currency.MarketClasses
+{
+    public interface IMarkets
+    {
+        List<CoinsList> GetCoinsList();
+    }
+}
