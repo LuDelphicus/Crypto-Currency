ee209f71296cde15b55f606cc8d8d0a617e77d3e
LuDelphic <sawa94378@gmail.com>
2023-02-08 20:38:55 +0300

    Install Newtonsoft.Json, Realized GetCoinList
    Some part of Realize GetCoinsList in CoinCap market

--- Crypto-Currency/MarketClasses/CoinCap.cs
+++ Crypto-Currency/MarketClasses/CoinCap.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Crypto_Currency.MarketClasses
 {
-    internal class CoinCap
+    public class CoinCap : IMarkets
     {
+        private string coinUri = "https://api.coincap.io/v2/assets/?limit=10";
+        private static HttpClient httpClient = new HttpClient();
+        public List<CoinsList> GetCoinsList()
+        {
+            var response = httpClient.GetAsync(coinUri).Result;
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
+            dynamic Data = responseData["data"];
+            return Data;
+        }
     }
 }
