7c51d8f2e8ce7ce074851e67487cacfe6b32889f
LuDelphic <38923850+LuDelphicus@users.noreply.github.com>
2023-02-12 00:13:26 +0300

    Create CoinsDetail, UriRespond, CoinsData method

--- Crypto-Currency/MarketClasses/CoinCap.cs
+++ Crypto-Currency/MarketClasses/CoinCap.cs
@@ -9,60 +9,25 @@ namespace Crypto_Currency.MarketClasses
     public class CoinCap : IMarkets
     {
         private string coinUri = "https://api.coincap.io/v2/assets/?limit=10";
+        public int id = 1;
 
         public static HttpClientHandler httpClientHandler = new HttpClientHandler();
         private static HttpClient httpClient = new HttpClient();
 
         public List<CoinsList> GetCoinsList() // Get list of top 10 coins
         {
-            var response = httpClient.GetAsync(coinUri).Result;
-            string responseBody = response.Content.ReadAsStringAsync().Result;
+            dynamic data = GetUriRespond(coinUri);
+            var list = GetCoinsData(data); // Create list of top 10 coins
 
-            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
-            dynamic data = responseData["data"];
-
-            var list = CreateCoinsList(data);
             return list;
-        }
-
-        private List<CoinsList> CreateCoinsList(dynamic data) // Create list of top 10 coins
-        {
-            int id = 1;
-            List<CoinsList> resultList = new List<CoinsList>();
-
-            foreach (var element in data)
-            {
-                string imgUri = $"https://assets.coincap.io/assets/icons/{((string)element.symbol).ToLower()}@2x.png";
-                float price = (float)Math.Round((double)element.priceUsd, 2);
-                float changing24H = (float)Math.Round((double)element.changePercent24Hr, 2);
-                string changing24hColor = (changing24H > 0) ? "#008A45" : "#CC1010";
-
-                resultList.Add(new CoinsList()
-                {
-                    Id = id,
-                    Name = (string)element.name,
-                    Symbol = (string)element.symbol,
-                    ImagePath = new BitmapImage(new Uri(imgUri)),
-                    Price = $"${price.ToString()}",
-                    Changin24h = $"{changing24H.ToString()}%",
-                    Chaning24hColor = changing24hColor
-                });
-
-                id++;
-            }
-            return resultList;
         }
 
         public dynamic GetCoinChanges(string coinName) // Get info about coin and changes of price
         {
             string coinChangesUri = $"https://api.coincap.io/v2/assets/{coinName.ToLower()}/history?interval=d1";
-            var response = httpClient.GetAsync(coinChangesUri).Result;
-            string responseBody = response.Content.ReadAsStringAsync().Result;
+            dynamic uriRespond = GetUriRespond(coinChangesUri);
 
-            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
-            dynamic data = responseData["data"];
-
-            var coinChanges = CreateCoinChanges(data);
+            var coinChanges = CreateCoinChanges(uriRespond);
             coinChanges.CoinInfo = GetCoinInfo(coinName);
 
             return coinChanges;
@@ -70,47 +35,19 @@ namespace Crypto_Currency.MarketClasses
 
         private dynamic GetCoinInfo(string coinName) // Get info about coin ( price, name, symbol, image )
         {
-            string coinChangesUri = $"https://api.coincap.io/v2/assets/{coinName.ToLower()}";
-            var response = httpClient.GetAsync(coinChangesUri).Result;
-            string responseBody = response.Content.ReadAsStringAsync().Result;
+            string coinUri = $"https://api.coincap.io/v2/assets/{coinName.ToLower()}";
+            dynamic uriRespond = GetUriRespond(coinUri);
+            var coinData = GetCoinsData(uriRespond);
 
-            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
-            dynamic respondData = responseData["data"];
-
-            var data = CreateCoinData(respondData);
-
-            return data;
-        }
-
-        private List<CoinsList> CreateCoinData(dynamic data) // Create data about coin ( price, name, symbol, image )
-        {
-            List<CoinsList> resultList = new List<CoinsList>();
-
-           
-            string imgUri = $"https://assets.coincap.io/assets/icons/{((string)data.symbol).ToLower()}@2x.png";
-            float price = (float)Math.Round((double)data.priceUsd, 2);
-            float changing24H = (float)Math.Round((double)data.changePercent24Hr, 2);
-            string changing24hColor = (changing24H > 0) ? "#008A45" : "#CC1010";
-
-            resultList.Add(new CoinsList()
-            {
-                Id = 1,
-                Name = (string)data.name,
-                Symbol = (string)data.symbol,
-                ImagePath = new BitmapImage(new Uri(imgUri)),
-                Price = $"${price.ToString()}",
-                Changin24h = $"{changing24H.ToString()}%",
-                Chaning24hColor = changing24hColor
-            });
-
-            return resultList;
+            return coinData;
         }
 
         private CoinChanges CreateCoinChanges(dynamic data) // Create changing list of coin price 
         {
             CoinChanges coinChanges = new CoinChanges();
-            float priceUsd;
 
+            float priceUsd;
+            
             foreach (var element in data)
             {
                 priceUsd = (float)element.priceUsd;
@@ -127,6 +64,57 @@ namespace Crypto_Currency.MarketClasses
             }
 
             return coinChanges;
+        }
+
+        private dynamic GetUriRespond(string Uri) // Return respond from URI
+        {
+            var response = httpClient.GetAsync(Uri).Result;
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+
+            var responseData = JsonConvert.DeserializeObject<dynamic>(responseBody);
+            dynamic data = responseData["data"];
+
+            return data;
+        }
+
+        private List<CoinsList> GetCoinsData(dynamic data) // Return detail of coins list ( price, name, symbol and etc )
+        {
+            List<CoinsList> resultList = new List<CoinsList>();
+
+            if (data.Count > 1)
+            {
+                foreach (var element in data)
+                {
+                    CreateCoinsDetail(element, resultList);
+                }
+
+                return resultList;
+            }
+           
+            CreateCoinsDetail(data, resultList);
+            return resultList;
+        }
+
+        private List<CoinsList> CreateCoinsDetail(dynamic data, List<CoinsList> resultList)
+        {
+            string imgUri = $"https://assets.coincap.io/assets/icons/{((string)data.symbol).ToLower()}@2x.png";
+            float price = (float)Math.Round((double)data.priceUsd, 2);
+            float changing24H = (float)Math.Round((double)data.changePercent24Hr, 2);
+            string changing24hColor = (changing24H > 0) ? "#008A45" : "#CC1010";
+
+            resultList.Add(new CoinsList()
+            {
+                Id = id,
+                Name = (string)data.name,
+                Symbol = (string)data.symbol,
+                ImagePath = new BitmapImage(new Uri(imgUri)),
+                Price = $"${price.ToString()}",
+                Changin24h = $"{changing24H.ToString()}%",
+                Chaning24hColor = changing24hColor
+            });
+            id++;
+
+            return resultList;
         }
     }
 }
